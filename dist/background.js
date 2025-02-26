23f05754c20c23310ce3b62a8aaf7d59aecad32c
alokraj-05 <alok200504@gmail.com>
2025-02-27 04:49:25 +0530

    Panel integration

--- dist/background.js
+++ dist/background.js
@@ -1,2 +1,15 @@
 "use strict";
 console.log("hello world!");
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'openSidePanel',
+        title: 'Open side panel',
+        contexts: ['all']
+    });
+});
+chrome.runtime.onMessage.addListener((message, sender) => {
+    var _a;
+    if (message.action === "summarise" && ((_a = sender.tab) === null || _a === void 0 ? void 0 : _a.id) !== undefined) {
+        chrome.sidePanel.open({ tabId: sender.tab.id });
+    }
+});
--- dist/popup.js
+++ dist/popup.js
@@ -1,9 +1,22 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("switch1");
     const switch1 = document.getElementById("base_switch");
-    var isEnabled = false;
-    if (toggleButton) {
+    if (toggleButton && switch1) {
+        chrome.storage.local.get("bwMode", (data) => {
+            const isEnabled = data.bwMode || false;
+            switch1.classList.toggle("fa-toggle-on", isEnabled);
+            switch1.classList.toggle("fa-toggle-off", !isEnabled);
+        });
         toggleButton.addEventListener("click", () => {
             chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
                 if (tabs[0].id) {
@@ -13,5 +26,23 @@ document.addEventListener("DOMContentLoaded", () => {
             switch1 === null || switch1 === void 0 ? void 0 : switch1.classList.toggle("fa-toggle-on");
             switch1 === null || switch1 === void 0 ? void 0 : switch1.classList.toggle("fa-toggle-off");
         });
+    }
+    const summariseBtn = document.getElementById("sum-btn");
+    if (summariseBtn) {
+        summariseBtn.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+            const [tab] = yield chrome.tabs.query({ active: true, currentWindow: true });
+            if (tab.id !== undefined) {
+                try {
+                    yield chrome.sidePanel.open({ tabId: tab.id });
+                    console.log("side panel opend successfully.");
+                }
+                catch (error) {
+                    console.error("error opening side panel:", error);
+                }
+            }
+            else {
+                console.error("no active tab found.");
+            }
+        }));
     }
 });
